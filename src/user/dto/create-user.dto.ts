2473d371a5074363d64adc54205f04f2500fd7fb
WYGIN <wygininc@gmail.com>
2024-06-04 08:19:02 +0530

    generate user table
    Signed-off-by: WYGIN <wygininc@gmail.com>

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsDate, IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString({ message: 'invalid FirstName'})
+    readonly firstName: string;
+
+    @IsString({ message: 'invalid LastName'})
+    readonly lastName: string;
+
+    @IsEmail()
+    readonly email: string;
+
+    @IsStrongPassword({ minLength: 8 })
+    readonly password: string
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+import { 
+    Entity, 
+    Column, 
+    PrimaryGeneratedColumn, 
+    CreateDateColumn, 
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+  
+    @Column()
+    lastName: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+}
